3bd23094b955568727ef1524f5ac1007548b9fdb
newbee <newbee@newbee-MacBookPro>
2016-09-19 09:55:58 +1200

    incremental version update

--- src/application/Main.java
+++ src/application/Main.java
@@ -59,7 +59,7 @@ public class Main extends Application implements MainInterface {
 		this._stage = primaryStage;
 		buildMainScenes();
 		try {
-			primaryStage.setTitle("VoxSpell v0.9.0-b");
+			primaryStage.setTitle("VoxSpell v0.9.1-b");
 			requestSceneChange("mainMenu");
 			primaryStage.show();
 			
